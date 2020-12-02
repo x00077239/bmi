a2fce75a002534b69ff21b4ec8f4b189947bc89f
Simon Monks <x00077239@mytudublin.ie>
2020-12-02 19:37:25 +0000

    finished unit testing

--- SampleNUnit Test Project/UnitTest1.cs
+++ SampleNUnit Test Project/UnitTest1.cs
@@ -31,7 +31,7 @@ namespace SampleNUnit_Test_Project
             Assert.AreEqual(ExpectedBMI, ActualBMI);
         }
         [Test]
-        public void TestBMINormal()
+        public void TestBMINormal() // unit test for Normal BMI function 
         {
             BMICalculator.BMI BMITest = new BMICalculator.BMI
             {
@@ -41,6 +41,51 @@ namespace SampleNUnit_Test_Project
                 HeightInches = 72,
             };
             BMICategory expected = BMICategory.Normal;
+            BMICategory actual = BMITest.BMICategory;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestBMIOverweight() //unit test for Overweight BMI function
+        {
+            BMICalculator.BMI BMITest = new BMICalculator.BMI
+            {
+                WeightStones = 12,
+                WeightPounds = 56,
+                HeightFeet = 6,
+                HeightInches = 82,
+            };
+            BMICategory expected = BMICategory.Overweight;
+            BMICategory actual = BMITest.BMICategory;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestBMIObese() // unit test for Obese BMI function 
+        {
+            BMICalculator.BMI BMITest = new BMICalculator.BMI
+            {
+                WeightStones = 2,
+                WeightPounds = 56,
+                HeightFeet = 4,
+                HeightInches = 72,
+            };
+            BMICategory expected = BMICategory.Obese;
+            BMICategory actual = BMITest.BMICategory;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestBMIUnderweight() // unit test for Underweight BMI function 
+        {
+            BMICalculator.BMI BMITest = new BMICalculator.BMI
+            {
+                WeightStones = 2,
+                WeightPounds = 56,
+                HeightFeet = 4,
+                HeightInches = 72,
+            };
+            BMICategory expected = BMICategory.Underweight;
             BMICategory actual = BMITest.BMICategory;
 
             Assert.AreEqual(expected, actual);
